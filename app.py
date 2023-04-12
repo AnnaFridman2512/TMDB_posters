e6d012f9c26d8d8c4a8686a44f6990f900c20e6b
Anna-Fridman <annafridman81@gmail.com>
2023-04-12 10:17:44 +0300

    find_movie function modifyied - redundancy  removed

--- app.py
+++ app.py
@@ -16,17 +16,13 @@ def find_movie():
     movie = find_poster(title)
     if type(movie) == dict: #If we found the movie in mongo we get a dict as a return value
         response = make_response('<h1>Movie {} found in Mongo</h1><img src="data:image/jpeg;base64,{}">'.format(movie['movie_title'],base64.b64encode(movie['poster']).decode()))
-        response.headers['Content-Type'] = 'text/html'
-        return response
     elif "https" in movie[1]:#if we found the movie in TMDB we get a list [movie_title, poster_url]
         movie_title, poster_url = movie
         response = make_response('<h1>Movie {} found in TMDB</h1><img src="{}">'.format(movie_title, poster_url))
-        response.headers['Content-Type'] = 'text/html'
-        return response
     else:#if movie isn't found - just show the error message that we resive from find_poster function (if not movies_list:)
         response = make_response('<h1>{}</h1>'.format(movie))
-        response.headers['Content-Type'] = 'text/html'
-        return response
+    response.headers['Content-Type'] = 'text/html'
+    return response
 
 
 if __name__ == '__main__':
