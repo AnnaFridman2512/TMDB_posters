9e610748e204c8d7db63402c560466591e220db3
Lior <khturh2@gmail.com>
2023-04-15 21:32:53 +0300

    Added user file and instructions

--- connect_to_mongoDB.py
+++ connect_to_mongoDB.py
@@ -1,42 +1,68 @@
+import base64
 import pymongo
 import requests
 import io
-from PIL import Image
-from passwords_and_keys import mongo_db_password as mpw
+from passwords_and_keys import mongo_db_password, username
 
 
-username = "user"
-password = mpw
+password = mongo_db_password
 
 # build the connection URI
-uri = f'mongodb://{username}:{mpw}@localhost:27017/TMDB_posters'
+uri = f'mongodb://localhost:27017/TMDB_posters'
 
 # connect to the database using the URI
 client = pymongo.MongoClient(uri)
-#client = pymongo.MongoClient("mongodb://localhost:27017/")
 
 # access a collection
 db = client["TMDB_posters"]
 collection = db["posters"]
 
+
 def create_mongo_user():
-    # check if user exists
+    # Check if user exists
     users_dict = db.command("usersInfo")
     users_list = users_dict['users']
-    print(users_list)
 
     if len(users_list) > 0:
         for user_dict in users_list:
             user_list_tup = user_dict.items()
             for user_data in user_list_tup:
-                if user_data[0] == username and user_data[1] == username:
+                if user_data[0] == username:
                     print(f'User {username} already exists')
     else:
-        # create user with read/write permissions to 'posters' collection
-        db.command("createUser", "user", pwd=mpw, roles=[{"role": "readWrite", "db": "TMDB_posters"}])
+        # Create user with read/write permissions to 'TMDB_posters' database
+        db.command("createUser", username, pwd=password, roles=[{"role": "readWrite", "db": "TMDB_posters"}])
         print(f'User {username} created successfully')
 
+        # Update the URI with the new credentials
+    updated_uri = f'mongodb://{username}:{password}@localhost:27017/TMDB_posters'
+    return updated_uri
 
+
+#create_mongo_user()
+def delete_mongo_user(username):
+    db.command("dropUser", username)
+    print(f"User {username} has been deleted.")
+
+#delete_mongo_user('user')
+
+def get_all_posters():
+    create_mongo_user()
+    posters = list(collection.find())
+    posters_dict = {}
+    for poster in posters:
+        poster_dict = {
+            "movie_title": poster["movie_title"],
+            "TMDB_id": poster["TMDB_id"],
+            "poster": base64.b64encode(poster["poster"]).decode('utf-8')
+        }
+        posters_dict.update({poster["movie_title"]: poster_dict})
+
+    #print(posters_dict)
+    return posters_dict
+
+
+#get_all_posters()
 def find_poster_in_mongo(movie_title):
     create_mongo_user()
     movie_title = movie_title.lower()
@@ -48,11 +74,7 @@ def find_poster_in_mongo(movie_title):
         return first_result
 
 
-
-#document = {'movie_title': 'Avatar'}
-#collection.insert_one(document)
-
-#find_poster_in_mongo("bla")
+# find_poster_in_mongo("bla")
 
 # define a function to save the image to MongoDB
 def save_poster_to_mongo(movie):
@@ -71,14 +93,13 @@ def save_poster_to_mongo(movie):
     collection.insert_one(document)
 
 
-
-movie = {'adult': False, 'backdrop_path': '/vL5LR6WdxWPjLPFRLe133jXWsh5.jpg', 'genre_ids': [28, 12, 14, 878],
-         'id': 19995, 'original_language': 'en', 'original_title': 'ANNA',
+"""movie = {'adult': False, 'backdrop_path': '/vL5LR6WdxWPjLPFRLe133jXWsh5.jpg', 'genre_ids': [28, 12, 14, 878],
+         'id': 19996, 'original_language': 'en', 'original_title': 'ANNA',
          'overview': 'In the 22nd century, a paraplegic Marine is dispatched to the moon Pandora on a unique mission, but becomes torn between following orders and protecting an alien civilization.',
          'popularity': 386.424, 'poster_path': '/jRXYjXNq0Cs2TcJjLkki24MLp7u.jpg', 'release_date': '2009-12-15',
-         'title': 'Avatar', 'video': False, 'vote_average': 7.569, 'vote_count': 28849}
+         'title': 'AnnaAnna', 'video': False, 'vote_average': 7.569, 'vote_count': 28849}
+save_poster_to_mongo(movie)"""
 
-save_poster_to_mongo(movie)
 
 def delete_poster_from_mongo(movie_title):
     create_mongo_user()
--- user data.sh
+++ user data.sh
@@ -1,5 +1,15 @@
 #! /bin/bash
 
+sudo yum update -y
+sudo yum install docker -y
+sudo service docker start
+sudo usermod -a -G docker ec2-user
+sudo chkconfig docker on
+sudo yum install git -y
+git clone https://github.com/AnnaFridman2512/TMDB_posters.git
+cd <your_repo_folder>
+sudo docker build -t <your_docker_image_name> .
+sudo docker run -p 5000:5000 -d <your_docker_image_name>
 
 
-#kjhkjhklkjlkhkjhk
+
