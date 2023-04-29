4f46018e456f32d5e897b259ee576a86d0b0e848
Anna-Fridman <annafridman81@gmail.com>
2023-04-29 20:18:16 +0300

    user-data modified

--- user-data.sh
+++ user-data.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
+
 yum update -y
 amazon-linux-extras install docker -y
 systemctl start docker
 systemctl enable docker
 usermod -a -G docker ec2-user
 
-yum install -y git
-git clone https://github.com/AnnaFridman2512/TMDB_posters.git
+# Install Docker Compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
-cd TMDB_posters
+# Clone repository and start application
+git clone https://github.com/AnnaFridman2512/TMDB_posters.git /TMDB_posters
+cd /TMDB_posters
 docker-compose up -d
