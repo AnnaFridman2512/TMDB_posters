b4b7abf142572a2f2f6f9e6fbe463d5a7a765c3e
Anna-Fridman <annafridman81@gmail.com>
2023-04-30 08:55:53 +0300

    user data modified

--- user-data.sh
+++ user-data.sh
@@ -1,16 +1,31 @@
 #!/bin/bash
 
-yum update -y
-amazon-linux-extras install docker -y
-systemctl start docker
-systemctl enable docker
-usermod -a -G docker ec2-user
+# Update the system packages
+sudo yum update -y
+
+# Install Python 3 and pip3
+sudo yum install python3 python3-pip -y
+
+# Install additional development libraries required by some Python packages
+sudo yum install gcc python3-devel openssl-devel -y
+
+# Install Git
+sudo yum install git -y
+
+# Install Docker
+sudo amazon-linux-extras install docker -y
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo usermod -a -G docker ec2-user
 
 # Install Docker Compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
-# Clone repository and start application
-git clone git@github.com:AnnaFridman2512/TMDB_posters.git /TMDB_posters
+# Clone the repository and install the dependencies
+git clone https://github.com/AnnaFridman2512/TMDB_posters.git /TMDB_posters
 cd /TMDB_posters
-docker-compose up -d
+sudo pip3 install -r requirements.txt
+
+# Start the application using Docker Compose
+sudo docker-compose up -d
